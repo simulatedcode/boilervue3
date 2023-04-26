64fedb9080847e6b020a78eaa1caaeb243f86317
simulatedcode <simulasi.studio@gmail.com>
2023-04-26 20:50:47 +0700

    add(antfu): Unplugin & UnoCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import autoImport from 'unplugin-auto-import/vite'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components(),
+    autoImport(
+      {
+        imports: [
+          'vue',
+        ]
+      }
+    ),
+    UnoCSS(),
+  ],
 })
